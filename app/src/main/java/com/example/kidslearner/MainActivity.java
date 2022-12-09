8d488d6be0941fa8709f41681e325924a02d0c69
Muhammad Amaan <bitf19m016@pucit.edu.pk>
2022-12-09 17:30:50 +0500

    Essentital elements added

--- app/src/main/java/com/example/kidslearner/MainActivity.java
+++ app/src/main/java/com/example/kidslearner/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.kidslearner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button SkyBtn, GrassBtn, RootBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SkyBtn = findViewById(R.id.btnSky);
+        RootBtn = findViewById(R.id.btnRoot);
+        GrassBtn = findViewById(R.id.btnGrass);
+
+        SkyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+        RootBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        GrassBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
